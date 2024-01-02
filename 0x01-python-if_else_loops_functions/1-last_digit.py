dc10b975298a580c3133465448ab7b4b72d8417a
kimulu <kimulumichael@gmail.com>
2024-01-02 07:03:39 -0800

    new commit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -11,8 +11,8 @@ else:
     message = (f"Last digit of {number} is {last_digit}")
 
 if last_digit > 5:
-   print(f"{message} and is greater than 5")
+    print(f"{message} and is greater than 5")
 elif last_digit == 0:
-   print(f"{message} and is 0")
+    print(f"{message} and is 0")
 elif last_digit < 6:
-   print(f"{message} and is less than 6 and not 0")
+    print(f"{message} and is less than 6 and not 0")
