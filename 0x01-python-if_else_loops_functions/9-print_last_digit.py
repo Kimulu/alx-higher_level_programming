dd0e42ffbbeaeb54cc124ae1d9ad0e37a6deefc8
kimulu <kimulumichael@gmail.com>
2024-01-02 10:27:00 -0800

    new commit

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    # Ensure the number is positive
+    number = abs(number)
+
+    # Get the last digit using modulo 10
+    last_digit = number % 10
+
+    # Print the last digit
+    print(last_digit)
+
+    # Return the value of the last digit
+    return last_digit
