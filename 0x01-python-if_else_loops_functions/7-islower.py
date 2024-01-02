6a46e7763c0306ea19b1b58a4b3c7f071ce6f0b0
kimulu <kimulumichael@gmail.com>
2024-01-02 09:43:47 -0800

    new commit

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def islower(c):
-    try:
-        if len(c) > 0:
-            # Check if the character is a lowercase letter
-            return 'a' <= c <= 'z'
-    except Exception as e:
-        return f'Traceback: {str(e)}'
+    return 97 <= ord(c) <= 122
