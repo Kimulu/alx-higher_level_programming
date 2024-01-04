10a545939196131bcdae819be87a18d2d76786d4
kimulu <kimulumichael@gmail.com>
2024-01-04 06:24:09 -0800

    new commit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,23 +1,20 @@
 #!/usr/bin/python3
-from sys import argv
+import sys
 
 def print_arguments():
     # Get the number of arguments
-    num_arguments = len(argv) - 1  # Subtract 1 to exclude the script name
+    num_arguments = len(sys.argv) - 1
 
-    # Print the number of arguments
     if num_arguments == 0:
         print("0 arguments.")
     elif num_arguments == 1:
         print("1 argument:")
+        print("1: Hello")
     else:
-        print(f"{num_arguments} arguments:")
-
-        # Print each argument with its position
-        for i in range(1, num_arguments + 1):
-            print(f"{i}: {argv[i]}")
+        print("{} arguments:".format(count))
+        for j in range(num_arguments):
+            print("{}: {}".format(i + 1,sys.argv[j + 1]))
 
 # Call the function to print the arguments
 if __name__ == "__main__":
     print_arguments()
-
