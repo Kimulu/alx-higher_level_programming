ced3a57d296de9579aef63a2e1c37fd13d37cc3a
kimulu <kimulumichael@gmail.com>
2024-01-04 06:01:38 -0800

    new commit

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -5,12 +5,7 @@ if __name__ == "__main__":
     a = 10
     b = 5
 
-    sum_result = add(a, b)
-    diff_result = sub(a, b)
-    product_result = mul(a, b)
-    quotient_result = div(a, b)
-
-    print(f"{a} + {b} = {sum_result}")
-    print(f"{a} - {b} = {diff_result}")
-    print(f"{a} * {b} = {product_result}")
-    print(f"{a} / {b} = {quotient_result}")
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
