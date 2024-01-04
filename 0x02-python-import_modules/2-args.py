1a59a43490ba426b66d0261bea9c42e1bc0331c8
kimulu <kimulumichael@gmail.com>
2024-01-04 06:29:09 -0800

    new commit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,20 +1,15 @@
 #!/usr/bin/python3
-import sys
 
-def print_arguments():
-    # Get the number of arguments
+if __name__ == "__main__":
+    import sys
+
     num_arguments = len(sys.argv) - 1
-
     if num_arguments == 0:
         print("0 arguments.")
     elif num_arguments == 1:
         print("1 argument:")
         print("1: Hello")
     else:
-        print("{} arguments:".format(count))
-        for j in range(num_arguments):
-            print("{}: {}".format(i + 1,sys.argv[j + 1]))
-
-# Call the function to print the arguments
-if __name__ == "__main__":
-    print_arguments()
+        print("{} arguments:".format(num_arguments))
+        for i in range(num_arguments):
+            print("{}: {}".format(i + 1, sys.argv[i + 1]))
