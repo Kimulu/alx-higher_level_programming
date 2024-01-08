665e6af7ff9d0dce8bcfae5120f2355cc6dc0bf0
kimulu <kimulumichael@gmail.com>
2024-01-07 22:38:30 -0800

    new commit

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+
 def print_list_integer(my_list=[]):
-    for number in my_list:
-        print("{}".format(number))
+    i = 0
+    while i < len(my_list):
+        print("{:d}".format(my_list[i]))
+        i = i + 1
