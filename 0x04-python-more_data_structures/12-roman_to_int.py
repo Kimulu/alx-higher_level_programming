35d0a6840bfe8a983a2873ed8a5ce49911ea6188
kimulu <kimulumichael@gmail.com>
2024-01-10 05:10:49 -0800

    new commit

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,39 +1,21 @@
-#!/usr/bin/python3
-def to_subtract(subtract_list):
-    to_subtract_value = 0
-    max_value = max(subtract_list)
-
-    for n in subtract_list:
-        if max_value > n:
-            to_subtract_value += n
-
-    return max_value - to_subtract_value
-
 def roman_to_int(roman_string):
-    if not roman_string:
+    if not isinstance(roman_string, str) or roman_string is None:
         return 0
 
-    if not isinstance(roman_string, str):
-        return 0
+    roman_numerals = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                      'C': 100, 'D': 500, 'M': 1000}
+    total_value = 0
+    previous_value = 0
 
-    roman_numerals = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    roman_keys = list(roman_numerals.keys())
+    for char in reversed(roman_string):
+        current_value = roman_numerals[char]
 
-    result = 0
-    last_roman_value = 0
-    subtract_list = [0]
+        if current_value < previous_value:
+            total_value -= current_value
+        else:
+            total_value += current_value
 
-    for char in roman_string:
-        for roman_numeral in roman_keys:
-            if roman_numeral == char:
-                if roman_numerals.get(char) <= last_roman_value:
-                    result += to_subtract(subtract_list)
-                    subtract_list = [roman_numerals.get(char)]
-                else:
-                    subtract_list.append(roman_numerals.get(char))
+        previous_value = current_value
 
-                last_roman_value = roman_numerals.get(char)
+    return total_value
 
-    result += to_subtract(subtract_list)
-
-    return result
