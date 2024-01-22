8dbf08d527e6752605fb8f502b4d25a1676bff98
kimulu <kimulumichael@gmail.com>
2024-01-22 01:45:13 -0800

    new commit

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def safe_print_integer(value):
+    try:
+        if isinstance(value, int):
+            print("{:d}".format(value))
+            return True
+    except ValueError:
+        pass
+    finally:
+        print()
+    return False
