03781c6fc922b48f16052070ca8b6d7df902226d
kimulu <kimulumichael@gmail.com>
2024-01-22 01:28:54 -0800

    new commit

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     printed_elements = 0
     try:
         for i in range(x):
-            print(my_list[i], end=' ')
+            print(my_list[i], end='')
             printed_elements += 1
     except IndexError:
         pass
