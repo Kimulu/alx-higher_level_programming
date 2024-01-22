6cf6bef1d8524ddf9ffee1ee5f15b1702f47bc32
kimulu <kimulumichael@gmail.com>
2024-01-22 01:57:46 -0800

    new commit

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    num_printed = 0
+    try:
+        for i in range(x):
+            if isinstance(my_list[i], int):
+                print("{:d}".format(my_list[i]), end="")
+                num_printed += 1
+        print()  # Add a newline after printing all integers
+    except IndexError:
+        print("list index out of range")
+    finally:
+        return num_printed
