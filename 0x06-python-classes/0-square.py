22fb8df9a5a9c29aea25d6dbbf2d3e868c208c22
kimulu <kimulumichael@gmail.com>
2024-01-23 07:08:32 -0800

    new commit

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,24 @@
 #!/usr/bin/python3
 
 class Square:
-	def __init__(self):
-		pass
+    """
+    Represents a square.
+
+    Attributes:
+        None.
+
+    Methods:
+        __init__(): Initializes a new instance of the Square class.
+    """
+
+    def __init__(self):
+        """
+        Initializes a new instance of the Square class.
+
+        Parameters:
+            None.
+
+        Returns:
+            None.
+        """
+        pass
