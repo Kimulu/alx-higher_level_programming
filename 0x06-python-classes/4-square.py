2d08f3149c161899a0f5be081f4fbb9466d243cd
kimulu <kimulumichael@gmail.com>
2024-01-23 23:53:48 -0800

    Access and update private attribute

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+"""Define a class Square."""
+
+
+class Square:
+    """Defines a square with private instance attribute 'size'."""
+
+    def __init__(self, size=0):
+        """Instantiates a new Square.
+
+        Args:
+        - size (int, optional): The size of the square. Defaults to 0.
+        """
+        self.size = size  # Utilizing the property setter for validation
+
+    @property
+    def size(self):
+        """Getter method to retrieve the size."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter method to set the size with validation.
+
+        Args:
+        - value (int): The new size to be set.
+
+        Raises:
+        - TypeError: If value is not an integer.
+        - ValueError: If value is less than 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """Calculates and returns the area of the square."""
+        return self.__size ** 2
