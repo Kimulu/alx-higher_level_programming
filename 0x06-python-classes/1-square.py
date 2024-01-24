70a0bc6fa0cb7e5585ecf13067a6a20dcc1a7f4b
kimulu <kimulumichael@gmail.com>
2024-01-23 22:06:36 -0800

    Square with size

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,12 +3,12 @@
 """Create a class named Square to model and handle square shapes."""
 
 class Square:
-"""This class serves as a representation of a square."""
+    """This class serves as a representation of a square."""
 
-def __init__(self, size):
-    """Instantiate a new Square object.
+	def __init__(self, size):
+            """Instantiate a new Square object.
 
-    Parameters:
-    - size (int): The dimension of the square's sides.
-    """
-    self.__size = size
+	    Parameters:
+	    - size (int): The dimension of the square's sides.
+	    """
+	    self.__size = size
