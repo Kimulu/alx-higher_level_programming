a93e3b08cdfb5bf44fef1fcb2278ffae46c77a7c
kimulu <kimulumichael@gmail.com>
2024-02-05 06:00:52 -0800

    new commit

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+""" class MyList that inherits from list """
+
+
+class MyList(list):
+    """
+    Customized list class (inherits from built-in list) with additional functionality.
+
+    Public Methods:
+    - print_sorted(self): Prints the list in ascending order.
+
+    Note: Assumes that all elements of the list are of type int.
+
+    Example Usage:
+    >>> my_list = MyList([3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5])
+    >>> print(my_list)
+    [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]
+    >>> my_list.print_sorted()
+    [1, 1, 2, 3, 3, 4, 5, 5, 5, 6, 9]
+    """
+
+    def print_sorted(self):
+        """
+        Prints the list in ascending order.
+
+        Returns:
+        None
+        """
+        # Print the sorted list (ascending order)
+        sorted_list = sorted(self)
+        print(sorted_list)
