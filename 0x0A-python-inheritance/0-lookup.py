96437c6dee9520bb829e8299c284ff6e168fe7bc
kimulu <kimulumichael@gmail.com>
2024-02-05 05:49:26 -0800

    new commit

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,26 +1,7 @@
 #!/usr/bin/python3
-
-""" function that returns the list of available attributes and methods"""
+"""Defines an object attribute lookup function."""
 
 
 def lookup(obj):
-    """
-    Returns a list of non-hidden attributes and methods of an object.
-
-    Parameters:
-    - obj: The object for which attributes and methods are to be looked up.
-
-    Returns:
-    A list of attribute and method names.
-    """
-    # Get the list of all attributes and methods of the object
-    all_attributes_and_methods = dir(obj)
-    # Initialize an empty list to store filtered attributes and methods
-    filtered_attributes_and_methods = []
-    # Iterate through all attributes and methods
-    for attr in all_attributes_and_methods:
-        if not callable(getattr(obj, attr)) or not attr.startswith("__"):
-            # Add the attribute or method to the filtered list
-            filtered_attributes_and_methods.append(attr)
-
-    return filtered_attributes_and_methods
+    """Return a list of an object's available attributes."""
+    return (dir(obj))
